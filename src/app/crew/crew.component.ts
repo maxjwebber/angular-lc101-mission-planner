a0bb6401480d40ce718e9921a5689ea084d7d3a6
Angie <place@holder.com>
2021-08-30 20:35:08 -0400

    dynamic crew list with edit/add/remove

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -12,10 +12,39 @@ export class CrewComponent implements OnInit {
     {name: "Mae Jemison", firstMission: false},
     {name: "Ellen Ochoa", firstMission: true}
   ];
+  astronautBeingEdited: object = null;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  add(newName: string, checked: boolean)
+  {
+    let index = 0
+    for (; index < this.crew.length; index++) {
+      if(this.crew[index]['name'] === newName)
+        break;
+    }
+    if (index===this.crew.length)
+      this.crew.push({name: newName,firstMission: checked})
+  }
+
+  remove(removeMe: object)
+  {
+    let index = this.crew.indexOf(removeMe);
+    if (index !== -1)
+      this.crew.splice(index,1);
+  }
+
+  edit(astronaut: object) {
+    this.astronautBeingEdited = astronaut;
+ }
+
+  save(newName: string, astronaut: object)
+  {
+    astronaut['name'] = newName;
+    this.astronautBeingEdited = null;
   }
 
 }
